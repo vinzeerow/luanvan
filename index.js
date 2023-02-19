d7214631e9edcd6702d33d5952257744c08841a3
vinzeerow <tpv2001@gmail.com>
2023-02-19 18:10:08 +0700

    Add new project food

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // react-native-reanimated
 // react-native-safe-area-context
 // react-native-screens
-// react-native-gesture-handler
+
 import 'react-native-gesture-handler'
 import {AppRegistry} from 'react-native';
 import App from './App';
